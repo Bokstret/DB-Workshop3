c7ab5315f637b0bc3137cd2f10444fa5c0c87aa7
Bokstret <43569571+Bokstret@users.noreply.github.com>
2020-05-02 21:27:34 +0300

    Данные исходного csv нужно было отредактировать.
    Поскольку в некоторых полях файла содержалась некорректная информация, пришлось их попросту удалить, а точнее, создать новый файл без них.

---
+++ CsvCleaner.py
@@ -0,0 +1,10 @@
+import csv
+input = open('projects.csv', 'rt')
+output = open('projects2.csv', 'wt',newline='')
+writer = csv.writer(output)
+for row in csv.reader(input):
+    
+    if (len(row[7]) >= 2) or (row[8] != ''):
+        writer.writerow(row)
+input.close()
+output.close()
