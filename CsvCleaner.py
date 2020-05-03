f4f7453d41d2c4073d4a99c2b80b2df64898e1f6
Bokstret <43569571+Bokstret@users.noreply.github.com>
2020-05-03 13:41:40 +0300

    Update CsvCleaner.py

--- CsvCleaner.py
+++ CsvCleaner.py
@@ -4,7 +4,7 @@ output = open('projects2.csv', 'wt',newline='')
 writer = csv.writer(output)
 for row in csv.reader(input):
     
-    if (len(row[7]) >= 2) or (row[8] != ''):
+    if (len(row[7]) >= 2) or (row[8] == ''):
         writer.writerow(row)
 input.close()
 output.close()
